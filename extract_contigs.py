efbb12c4643872198a49c8b7d47ed5bba2886cbf
paul wolf <paul.wolf@usu.edu>
2014-10-19 16:51:06 -0600

    Added optional output filname choice as third argument for strip_contigs.py and extract_contigs.py

--- extract_contigs.py
+++ extract_contigs.py
@@ -1,9 +1,13 @@
 #! /usr/bin/env python
 
 """
+By Paul Wolf. Modified Oct 19th, 2014
+paul.wolf@usu.edu
+
 This script takes a fasta file (first argument) and a file containing a list of contigs
 that you want to extract (one conig name per line)- filename is second argument.
-output file (output.fasta) is jus those you want to extract
+output file (output.fasta) is just those you want to extract. Output file name shoule be 
+third argument, but will give default name if not specified
 
 """
 
@@ -11,10 +15,10 @@ from Bio import SeqIO
 import sys
 
 
-def extract_contigs(fasta_file, contigs_to_extract_list):
-    out = open('Ouput.fasta', 'w')
+def extract_contigs(fasta_file, contigs_to_extract_list, out_name):
+    out = open(out_name, 'w')
     for record in SeqIO.parse(fasta_file, "fasta") :
-        print record.id
+        #print record.id
         if record.id in contigs_to_extract_list:
             newline = str(record.id) + '\n' + str(record.seq) + ('\n')
             out.write(newline)
@@ -39,8 +43,12 @@ if __name__ == "__main__":
     fasta_file = open(sys.argv[1], 'rU)')
     contig_list_file = open(sys.argv[2],'r')
     contigs_to_extract_list = make_contig_list(contig_list_file)
-    print contigs_to_extract_list
-    extract_contigs(fasta_file,contigs_to_extract_list)
+    #print contigs_to_extract_list
+    if len(sys.argv) < 4:
+        outfile_name = 'extracted_output.fasta'
+    else:
+        outfile_name = sys.argv[3]
+    extract_contigs(fasta_file,contigs_to_extract_list, outfile_name)
     #print_lengths_cp_contigs(handle,cp_list)
     fasta_file.close()
     contig_list_file.close()
--- strip_contigs.py
+++ strip_contigs.py
@@ -1,9 +1,12 @@
 #! /usr/bin/env python
 
 """
+By Paul Wolf. Modified Oct 19th, 2014
+paul.wolf@usu.edu
 This script takes a fasta file (first argument) and a file containing a list of contigs
 that you want to remove (one conig name per line)- filename is second argument.
-output file (output.fasta) has contigs removed
+output file (output.fasta) has contigs removed. Output file name shoule be third argument,
+but will give default name if not specified
 
 """
 
@@ -11,10 +14,10 @@ from Bio import SeqIO
 import sys
 
 
-def remove_contigs(fasta_file, contigs_to_remove_list):
-    out = open('Ouput.fasta', 'w')
+def remove_contigs(fasta_file, contigs_to_remove_list, out_name):
+    out = open(out_name, 'w')
     for record in SeqIO.parse(fasta_file, "fasta") :
-        print record.id
+        #print record.id
         if record.id not in contigs_to_remove_list:
             newline = str(record.id) + '\n' + str(record.seq) + ('\n')
             out.write(newline)
@@ -39,8 +42,12 @@ if __name__ == "__main__":
     fasta_file = open(sys.argv[1], 'rU)')
     contig_list_file = open(sys.argv[2],'r')
     contigs_to_remove_list = make_contig_list(contig_list_file)
-    print contigs_to_remove_list
-    remove_contigs(fasta_file,contigs_to_remove_list)
+    #print contigs_to_remove_list
+    if len(sys.argv) < 4:
+        outfile_name = 'stripped_output.fasta'
+    else:
+        outfile_name = sys.argv[3]    
+    remove_contigs(fasta_file,contigs_to_remove_list, outfile_name)
     #print_lengths_cp_contigs(handle,cp_list)
     fasta_file.close()
     contig_list_file.close()
